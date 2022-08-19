26241875d48661ff067b3ad8a2900900f181a004
Matija Martinic <matija.martinic@gmail.com>
2022-08-19 21:52:46 +0200

    save

--- chain/evm/health_check.go
+++ chain/evm/health_check.go
@@ -10,6 +10,7 @@ func (p Processor) HealthCheck(ctx context.Context) error {
 	if len(p.evmClient.addr) == 0 {
 		return chain.ErrMissingAccount.Format(p.chainReferenceID)
 	}
+
 	balance, err := p.evmClient.BalanceAt(ctx, p.evmClient.addr, 0)
 	if err != nil {
 		return err
