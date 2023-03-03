b6978e51ab216f083299b3b520637cab71d9afce
webelf101 <60479748+webelf101@users.noreply.github.com>
2023-03-04 03:51:11 +0800

    fix fetch event bug (#104)

--- chain/evm/mock_evmClienter_test.go
+++ chain/evm/mock_evmClienter_test.go
@@ -175,6 +175,32 @@ func (_m *mockEvmClienter) TransactionByHash(ctx context.Context, txHash common.
 	return r0, r1, r2
 }
 
+// FindCurrentBlockNumber provides a mock function with given fields: ctx
+func (_m *mockEvmClienter) FindCurrentBlockNumber(ctx context.Context) (*big.Int, error) {
+	ret := _m.Called(ctx)
+
+	var r0 *big.Int
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) (*big.Int, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) *big.Int); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*big.Int)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 type mockConstructorTestingTnewMockEvmClienter interface {
 	mock.TestingT
 	Cleanup(func())
