4071da8e37c04d4d8c35a81c7356ad4b72958877
Tyler Ruppert <524638+ToasterTheBrave@users.noreply.github.com>
2023-06-01 14:01:50 -0600

    Fix and refactor some process logic (#173)
    Co-authored-by: Tyler Ruppert <{ID}+{username}@users.noreply.github.com>

--- relayer/process.go
+++ relayer/process.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/VolumeFi/whoops"
 	"github.com/palomachain/pigeon/chain"
 	"github.com/palomachain/pigeon/chain/paloma"
 	"github.com/palomachain/pigeon/chain/paloma/collision"
@@ -11,16 +12,115 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func (r *Relayer) Process(ctx context.Context, processors []chain.Processor) error {
-	if len(processors) == 0 {
-		return nil
+func (r *Relayer) SignMessages(ctx context.Context, queueName string, messagesForSigning []chain.QueuedMessage, processor chain.Processor) error {
+	loggerQueuedMessages := log.WithFields(log.Fields{
+		"queue-name": queueName,
+		"message-ids": slice.Map(messagesForSigning, func(msg chain.QueuedMessage) uint64 {
+			return msg.ID
+		}),
+	})
+
+	if len(messagesForSigning) > 0 {
+		loggerQueuedMessages.Info("messages to sign")
+		signedMessages, err := processor.SignMessages(ctx, queueName, messagesForSigning...)
+		if err != nil {
+			loggerQueuedMessages.WithError(err).Error("unable to sign messages")
+			return err
+		}
+		loggerQueuedMessages = loggerQueuedMessages.WithFields(log.Fields{
+			"signed-messages": slice.Map(signedMessages, func(msg chain.SignedQueuedMessage) log.Fields {
+				return log.Fields{
+					"id": msg.ID,
+				}
+			}),
+		})
+		loggerQueuedMessages.Info("signed messages")
+
+		if err = r.broadcastSignatures(ctx, queueName, signedMessages); err != nil {
+			loggerQueuedMessages.WithError(err).Error("couldn't broadcast signatures and process attestation")
+			return err
+		}
 	}
+	return nil
+}
+
+func (r *Relayer) RelayMessages(ctx context.Context, queueName string, messagesInQueue []chain.MessageWithSignatures, p chain.Processor) error {
+	logger := log.WithFields(log.Fields{
+		"queue-name": queueName,
+	})
 
 	ctx, cleanup, err := collision.GoStartLane(ctx, r.palomaClient, r.palomaClient.GetValidatorAddress())
 	if err != nil {
 		return err
 	}
 	defer cleanup()
+
+	relayCandidateMsgs := slice.Filter(
+		messagesInQueue,
+		func(msg chain.MessageWithSignatures) bool {
+			return len(msg.PublicAccessData) == 0
+		},
+		func(msg chain.MessageWithSignatures) bool {
+			return collision.AllowedToExecute(
+				ctx,
+				[]byte(fmt.Sprintf("%s-%d", queueName, msg.ID)),
+			)
+		},
+	)
+
+	if len(relayCandidateMsgs) > 0 {
+		logger := logger.WithFields(log.Fields{
+			"messages-to-relay": slice.Map(relayCandidateMsgs, func(msg chain.MessageWithSignatures) uint64 {
+				return msg.ID
+			}),
+		})
+		logger.Info("relaying messages")
+		err := p.ProcessMessages(ctx, queueName, relayCandidateMsgs)
+		if err != nil {
+			logger.WithFields(log.Fields{
+				"err":        err,
+				"queue-name": queueName,
+				"messages-to-relay": slice.Map(relayCandidateMsgs, func(msg chain.MessageWithSignatures) uint64 {
+					return msg.ID
+				}),
+			}).Error("error relaying messages")
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *Relayer) ProvideEvidenceForMessages(ctx context.Context, queueName string, messagesInQueue []chain.MessageWithSignatures, p chain.Processor) error {
+	logger := log.WithFields(log.Fields{
+		"queue-name": queueName,
+	})
+
+	msgsToProvideEvidenceFor := slice.Filter(messagesInQueue, func(msg chain.MessageWithSignatures) bool {
+		return len(msg.PublicAccessData) > 0
+	})
+
+	if len(msgsToProvideEvidenceFor) > 0 {
+		logger := logger.WithFields(log.Fields{
+			"messages-to-provide-evidence-for": slice.Map(msgsToProvideEvidenceFor, func(msg chain.MessageWithSignatures) uint64 {
+				return msg.ID
+			}),
+		})
+		logger.Info("providing evidence for messages")
+		err := p.ProvideEvidence(ctx, queueName, msgsToProvideEvidenceFor)
+		if err != nil {
+			logger.WithError(err).Error("error providing evidence for messages")
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *Relayer) Process(ctx context.Context, processors []chain.Processor) error {
+	var processErrors whoops.Group
+
+	if len(processors) == 0 {
+		return nil
+	}
 
 	// todo randomise
 	for _, p := range processors {
@@ -31,100 +131,41 @@ func (r *Relayer) Process(ctx context.Context, processors []chain.Processor) err
 				"queue-name": queueName,
 			})
 
-			// TODO: remove comments once signing is done on the paloma side.
-			queuedMessages, err := r.palomaClient.QueryMessagesForSigning(ctx, queueName)
-			loggerQueuedMessages := logger.WithFields(log.Fields{
-				"message-ids": slice.Map(queuedMessages, func(msg chain.QueuedMessage) uint64 {
-					return msg.ID
-				}),
-			})
-
-			if err != nil {
-				logger.Error("failed getting messages to sign")
-				return err
-			}
-
-			if len(queuedMessages) > 0 {
-				loggerQueuedMessages.Info("messages to sign")
-				signedMessages, err := p.SignMessages(ctx, queueName, queuedMessages...)
-				if err != nil {
-					loggerQueuedMessages.WithError(err).Error("unable to sign messages")
-					return err
-				}
-				loggerQueuedMessages = loggerQueuedMessages.WithFields(log.Fields{
-					"signed-messages": slice.Map(signedMessages, func(msg chain.SignedQueuedMessage) log.Fields {
-						return log.Fields{
-							"id": msg.ID,
-						}
-					}),
-				})
-				loggerQueuedMessages.Info("signed messages")
-
-				if err = r.broadcastSignatures(ctx, queueName, signedMessages); err != nil {
-					loggerQueuedMessages.WithError(err).Error("couldn't broadcast signatures and process attestation")
-					return err
-				}
-			}
-
-			msgsInQueue, err := r.palomaClient.QueryMessagesInQueue(ctx, queueName)
-
-			logger.Debug("got ", len(msgsInQueue), " messages from ", queueName)
-
-			relayCandidateMsgs := slice.Filter(
-				msgsInQueue,
-				func(msg chain.MessageWithSignatures) bool {
-					return len(msg.PublicAccessData) == 0
-				},
-				func(msg chain.MessageWithSignatures) bool {
-					return collision.AllowedToExecute(
-						ctx,
-						[]byte(fmt.Sprintf("%s-%d", queueName, msg.ID)),
-					)
-				},
-			)
-
-			msgsToProvideEvidenceFor := slice.Filter(msgsInQueue, func(msg chain.MessageWithSignatures) bool {
-				return len(msg.PublicAccessData) > 0
-			})
-
+			messagesInQueue, err := r.palomaClient.QueryMessagesInQueue(ctx, queueName)
+			logger.Debug("got ", len(messagesInQueue), " messages from ", queueName)
 			if err != nil {
 				logger.WithError(err).Error("couldn't get messages to relay")
 				return err
 			}
 
-			if len(relayCandidateMsgs) > 0 {
-				logger := logger.WithFields(log.Fields{
-					"messages-to-relay": slice.Map(relayCandidateMsgs, func(msg chain.MessageWithSignatures) uint64 {
-						return msg.ID
-					}),
-				})
-				logger.Info("relaying messages")
-				if err = p.ProcessMessages(ctx, queueName, relayCandidateMsgs); err != nil {
-					logger.WithFields(log.Fields{
-						"err":        err,
-						"queue-name": queueName,
-						"messages-to-relay": slice.Map(relayCandidateMsgs, func(msg chain.MessageWithSignatures) uint64 {
-							return msg.ID
-						}),
-					}).Error("error relaying messages")
-					return err
-				}
+			messagesForSigning, err := r.palomaClient.QueryMessagesForSigning(ctx, queueName)
+			if err != nil {
+				logger.Error("failed getting messages to sign")
+				return err
 			}
 
-			if len(msgsToProvideEvidenceFor) > 0 {
-				logger := logger.WithFields(log.Fields{
-					"messages-to-provide-evidence-for": slice.Map(msgsToProvideEvidenceFor, func(msg chain.MessageWithSignatures) uint64 {
-						return msg.ID
-					}),
-				})
-				logger.Info("providing evidence for messages")
-				if err = p.ProvideEvidence(ctx, queueName, msgsToProvideEvidenceFor); err != nil {
-					logger.WithError(err).Error("error providing evidence for messages")
-					return err
-				}
+			err = r.SignMessages(ctx, queueName, messagesForSigning, p)
+			if err != nil {
+				logger.Error("failed signing messages")
+				processErrors.Add(err)
 			}
 
+			err = r.ProvideEvidenceForMessages(ctx, queueName, messagesInQueue, p)
+			if err != nil {
+				logger.Error("failed providing evidence for messages")
+				processErrors.Add(err)
+			}
+
+			err = r.RelayMessages(ctx, queueName, messagesInQueue, p)
+			if err != nil {
+				logger.Error("failed relaying messages")
+				processErrors.Add(err)
+			}
 		}
+	}
+
+	if processErrors.Err() {
+		return processErrors
 	}
 
 	return nil
--- relayer/start.go
+++ relayer/start.go
@@ -3,11 +3,11 @@ package relayer
 import (
 	"context"
 	goerrors "errors"
+	"github.com/palomachain/pigeon/errors"
 	"sync"
 	"time"
 
 	"github.com/VolumeFi/whoops"
-	"github.com/palomachain/pigeon/errors"
 	"github.com/palomachain/pigeon/util/channels"
 	log "github.com/sirupsen/logrus"
 )
@@ -50,33 +50,7 @@ func (r *Relayer) Start(ctx context.Context) error {
 	log.Info("starting relayer")
 	var locker sync.Mutex
 
-	go func() {
-		ticker := time.NewTicker(defaultLoopTimeout)
-		defer ticker.Stop()
-
-		for range ticker.C {
-			processors, err := r.buildProcessors(ctx)
-			if err != nil {
-				log.WithFields(log.Fields{
-					"err": err,
-				}).Error("couldn't build processors to update external chain info")
-
-				continue
-			}
-
-			log.Info("trying to update external chain info")
-
-			locker.Lock()
-			err = r.updateExternalChainInfos(ctx, processors)
-			locker.Unlock()
-
-			if err != nil {
-				log.WithFields(log.Fields{
-					"err": err,
-				}).Error("couldn't update external chain info. Will try again.")
-			}
-		}
-	}()
+	go r.startUpdateExternalChainInfos(ctx, &locker)
 
 	ticker := time.NewTicker(defaultLoopTimeout)
 	defer ticker.Stop()
@@ -84,50 +58,6 @@ func (r *Relayer) Start(ctx context.Context) error {
 	// only used to enter into the loop below immediately after the first "tick"
 	firstLoopEnter := make(chan time.Time, 1)
 	firstLoopEnter <- time.Time{}
-
-	process := func() error {
-		log.Info("relayer loop")
-		if ctx.Err() != nil {
-			log.Info("exiting relayer loop as context has ended")
-			return ctx.Err()
-		}
-
-		processors, err := r.buildProcessors(ctx)
-		if err != nil {
-			return err
-		}
-
-		locker.Lock()
-		err = r.Process(ctx, processors)
-		locker.Unlock()
-
-		switch {
-		case err == nil:
-			// success
-			return nil
-		case goerrors.Is(err, context.Canceled):
-			log.WithFields(log.Fields{
-				"err": err,
-			}).Debug("exited from the process loop due the context being canceled")
-			return nil
-		case goerrors.Is(err, context.DeadlineExceeded):
-			log.WithFields(log.Fields{
-				"err": err,
-			}).Debug("exited from the process loop due the context deadline being exceeded")
-			return nil
-		case errors.IsUnrecoverable(err):
-			// there is no way that we can recover from this
-			log.WithFields(log.Fields{
-				"err": err,
-			}).Error("unrecoverable error returned")
-			return err
-		default:
-			log.WithFields(log.Fields{
-				"err": err,
-			}).Error("error returned in process loop")
-			return nil
-		}
-	}
 
 	go func() {
 		r.startKeepAlive(ctx, &locker)
@@ -147,9 +77,81 @@ func (r *Relayer) Start(ctx context.Context) error {
 				}
 				return whoops.WrapS(ErrUnknown, "ticker channel for message processing was closed unexpectedly")
 			}
-			if err := process(); err != nil {
+			if err := r.process(ctx, &locker); err != nil {
 				log.WithError(err).Error("error while trying to process messages")
 			}
 		}
+	}
+}
+
+func (r *Relayer) startUpdateExternalChainInfos(ctx context.Context, locker sync.Locker) {
+	ticker := time.NewTicker(defaultLoopTimeout)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		processors, err := r.buildProcessors(ctx)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"err": err,
+			}).Error("couldn't build processors to update external chain info")
+
+			continue
+		}
+
+		log.Info("trying to update external chain info")
+
+		locker.Lock()
+		err = r.updateExternalChainInfos(ctx, processors)
+		locker.Unlock()
+
+		if err != nil {
+			log.WithFields(log.Fields{
+				"err": err,
+			}).Error("couldn't update external chain info. Will try again.")
+		}
+	}
+}
+
+func (r *Relayer) process(ctx context.Context, locker sync.Locker) error {
+	log.Info("relayer loop")
+	if ctx.Err() != nil {
+		log.Info("exiting relayer loop as context has ended")
+		return ctx.Err()
+	}
+
+	processors, err := r.buildProcessors(ctx)
+	if err != nil {
+		return err
+	}
+
+	locker.Lock()
+	err = r.Process(ctx, processors)
+	locker.Unlock()
+
+	switch {
+	case err == nil:
+		// success
+		return nil
+	case goerrors.Is(err, context.Canceled):
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Debug("exited from the process loop due the context being canceled")
+		return nil
+	case goerrors.Is(err, context.DeadlineExceeded):
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Debug("exited from the process loop due the context deadline being exceeded")
+		return nil
+	case errors.IsUnrecoverable(err):
+		// there is no way that we can recover from this
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("unrecoverable error returned")
+		return err
+	default:
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("error returned in process loop")
+		return nil
 	}
 }
