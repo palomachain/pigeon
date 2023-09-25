d3babf45f46c340d956bc67882bd537d65a13f1d
Christian Lohr <christian@volume.finance>
2023-09-26 00:58:44 +0200

    fix: avoid race condition when building chain processors (#288)
    Co-authored-by: byte-bandit <christian@volume.finance>

--- relayer/update_valset.go
+++ relayer/update_valset.go
@@ -6,55 +6,50 @@ import (
 
 	"github.com/palomachain/pigeon/chain"
 	"github.com/palomachain/pigeon/chain/paloma"
+	"github.com/palomachain/pigeon/internal/liblog"
 	"github.com/palomachain/pigeon/internal/traits"
-	"github.com/palomachain/pigeon/util/slice"
 	log "github.com/sirupsen/logrus"
 )
 
 func (r *Relayer) UpdateExternalChainInfos(ctx context.Context, locker sync.Locker) error {
-	err := r.buildProcessors(ctx, locker)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"err": err,
-		}).Error("couldn't build processors to update external chain info")
+	logger := liblog.WithContext(ctx).WithField("component", "update-external-chain-infos")
+	logger.Info("updating external chain infos")
 
+	if err := r.buildProcessors(ctx, locker); err != nil {
+		logger.WithError(err).Error("couldn't build processors to update external chain info")
 		return err
 	}
 
-	log.Info("updating external chain infos")
-	externalAccounts := slice.Map(
-		r.processors,
-		func(p chain.Processor) chain.ExternalAccount {
-			return p.ExternalAccount()
-		},
-	)
+	externalAccounts := make([]chain.ExternalAccount, len(r.processors))
+	for i, v := range r.processors {
+		externalAccounts[i] = v.ExternalAccount()
+	}
 
-	// log.Info("Updating erc20 coin contract for chain")
-
-	chainInfos := slice.Map(externalAccounts, func(acc chain.ExternalAccount) paloma.ChainInfoIn {
-		traits := traits.Build(acc.ChainReferenceID, r.mevClient)
-		info := paloma.ChainInfoIn{
-			ChainReferenceID: acc.ChainReferenceID,
-			AccAddress:       acc.Address,
-			ChainType:        acc.ChainType,
-			PubKey:           acc.PubKey,
-			Traits:           traits,
-		}
-		log.WithFields(log.Fields{
-			"chain-reference-id": acc.ChainReferenceID,
-			"acc-address":        acc.Address,
-			"chain-type":         acc.ChainType,
+	chainInfos := make([]paloma.ChainInfoIn, len(externalAccounts))
+	for i, v := range externalAccounts {
+		traits := traits.Build(v.ChainReferenceID, r.mevClient)
+		logger.WithFields(log.Fields{
+			"chain-reference-id": v.ChainReferenceID,
+			"acc-address":        v.Address,
+			"chain-type":         v.ChainType,
 			"chain-traits":       traits,
 		}).Info("sending account info to paloma")
-		return info
-	})
 
-	if len(chainInfos) == 0 {
+		chainInfos[i] = paloma.ChainInfoIn{
+			ChainReferenceID: v.ChainReferenceID,
+			AccAddress:       v.Address,
+			ChainType:        v.ChainType,
+			PubKey:           v.PubKey,
+			Traits:           traits,
+		}
+	}
+
+	if len(chainInfos) < 1 {
 		return nil
 	}
 
 	locker.Lock()
-	err = r.palomaClient.AddExternalChainInfo(ctx, chainInfos...)
+	err := r.palomaClient.AddExternalChainInfo(ctx, chainInfos...)
 	locker.Unlock()
 
 	return err
